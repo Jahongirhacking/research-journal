b800428ed95be37c790d93f7bad12a14f4a9a0f7
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-03-18 16:00:15 +0500

    fix: remove unused chunks

--- src/pages/auth/RootLayout.tsx
+++ src/pages/auth/RootLayout.tsx
@@ -1,21 +1,14 @@
-import { Header } from '@/components/Header/Header';
-import { paths } from '@/router/paths';
-import { RootState } from '@/store/store';
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router-dom';
+
+import { Outlet } from 'react-router-dom';
 import './style.scss';
 
 const RootLayout = () => {
-  const access = useSelector((store: RootState) => store.authSlice.access);
+  // const access = useSelector((store: RootState) => store.authSlice.access);
 
-  if (access) return <Navigate to={paths.profile} />;
+  // if (access) return <Navigate to={paths.profile} />;
 
   return (
     <div className="login">
-      <div className="login__header">
-        <Header isLogin />
-      </div>
-
       <div className="login__main">
         <div
           className="login-card flex-center direction-column gap24"
