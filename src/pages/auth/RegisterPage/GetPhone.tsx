15f656cff0289021ca7061c9b03c2ce9d523189e
Jahongirhacking <Jahongirhacking@gmail.com>
2025-03-20 03:41:33 +0500

    feat: add main page components

--- src/pages/auth/RegisterPage/GetPhone.tsx
+++ src/pages/auth/RegisterPage/GetPhone.tsx
@@ -137,7 +137,7 @@ const GetPhone = ({ handleSubmit }: { handleSubmit: (data: object) => Promise<bo
                   </Form.Item>
                 </Flex>
 
-                <Flex gap={20} justify='center'>
+                <Flex gap={20} justify='center' align='center' wrap>
                   <Button className="main-btn one-id-btn">
                     ONE ID orqali kirish
                   </Button>
